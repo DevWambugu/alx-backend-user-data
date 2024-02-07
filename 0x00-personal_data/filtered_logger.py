f461919082be6657ecc156ae73059849feedb94c
DevWambugu <venusdata100@gmail.com>
2024-02-07 21:07:04 +0300

    upto task 4

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+'''filtered_logger'''
+
+
+import logging
+import re
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields, redaction, message, separator):
+    '''returns the log message obfuscated'''
+    for field in fields:
+        message = (rf"{field}=(.*?)\{separator}",
+                   f'{field}={redaction}{separator}', message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''This function formats the log record'''
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
+
+
+def get_logger() -> logging.Logger:
+    """ Implementing a logger.i
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """This function returns a connector to the database
+    """
+    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    database_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    connector = mysql.connector.connect(
+            host=host,
+            database=database_name,
+            user=username,
+            password=password)
+    return connector
+
+
+def main() -> None:
+    """ Implement a main function
+    """
+    database = get_db()
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        print(message)
+    cursor.close()
+    database.close()
