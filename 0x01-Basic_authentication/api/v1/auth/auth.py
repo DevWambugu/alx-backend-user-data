3d1472e679d4b0da9ece86de00f7ec66786942b0
DevWambugu <venusdata100@gmail.com>
2024-02-14 00:13:45 +0300

    check conditions

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 
 from flask import request
 from typing import List, TypeVar
+from tabnanny import check
 
 
 class Auth:
@@ -11,7 +12,14 @@ class Auth:
     the API authentication'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''returns False'''
-        return False
+        check = path
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            check += "/"
+        if check in excluded_paths or path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''returns None'''
